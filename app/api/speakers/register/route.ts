87bf81b3a7330b1ff170202789e91e6caba6eabe
AmalRoy55 <amal@ethan-ai.com>
2024-09-23 17:21:20 +0530

    api build

--- app/api/speakers/register/route.ts
+++ app/api/speakers/register/route.ts
@@ -4,19 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    // Parse the request body to get form data
     const formData = await request.json();
 
-    // You can validate the data here (Optional)
-    // For example, if you have a validation utility
     if (!validateSponsorsFormData(formData)) {
       return NextResponse.json({ error: 'Invalid data' }, { status: 400 });
     }
 
-    // Call the service to save form data to the database
     const result = await saveSpeakersForm(formData);
 
-    // Return a successful response
     return NextResponse.json(
       {
         message: "Form data saved successfully",
@@ -26,7 +21,6 @@ export async function POST(request: NextRequest) {
     );
   } catch (error) {
     console.error("Error saving form data:", error);
-    // Return an error response if something goes wrong
     return NextResponse.json(
       { error: "Failed to save form data" },
       { status: 500 }
